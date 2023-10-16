110794543a1bef3276374496c09f7ee93ea0dbe3
maruki.tanada <maruki.tanada@gmail.com>
2023-10-17 08:07:00 +0900

    getAllMessagesを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,12 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -20,15 +25,23 @@ public class IndexServlet extends HttpServlet {
      */
     public IndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+
+        //DAOインスタンス（Hibernateバージョン）である em を生成
+        EntityManager em = DBUtil.createEntityManager();
+
+        //DAOインスタンス(em)を使って、DBのmessagesテーブルから全データを取得してDTOインスタンスのリストに変換
+        //getAllMessages を createNamedQuery メソッドの引数に指定することで、データベースへの問い合わせを実行できるようになる
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        em.close();
     }
 
 }
